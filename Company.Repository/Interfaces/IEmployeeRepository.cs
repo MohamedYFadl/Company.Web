cf845f262e31458e6a77bc166273b0bc00947289
MomoYasser0 <myasoo28@gmail.com>
2024-08-31 07:36:55 +0300

    Repository Pattern & Employee Repository

---
+++ Company.Repository/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Company.Data.Entities;
+
+
+namespace Company.Repository.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetById(int id);
+        IEnumerable<Employee> GetAll();
+        void Add(Employee employee);
+        void Update(Employee employee);
+        void Delete(Employee employee);
+    }
+}
