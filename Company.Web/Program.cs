1663fa1f2d53a722604b982e0038e2a3c5a8b841
MomoYasser0 <myasoo28@gmail.com>
2024-09-21 14:24:24 +0300

    RolesController(Index & Create)

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -80,7 +80,7 @@ namespace Company.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Account}/{action=SignUp}");
+                pattern: "{controller=Home}/{action=Index}");
 
             app.Run();
         }
